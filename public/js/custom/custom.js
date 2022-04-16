593bcf4123536fd71a855f55185806fac8f0b23b
minuetchoi-study <minuetchoi@daum.net>
2022-04-16 21:20:44 +0900

    test

--- public/js/custom/custom.js
+++ public/js/custom/custom.js
@@ -427,7 +427,7 @@ function shuffle(array) {
 
 // 북마크
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("mySidenav").style.width = "200px";
 }
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
