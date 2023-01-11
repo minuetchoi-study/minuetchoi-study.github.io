0c10217b14243dddb3ba49c0f68a4177c31ed01c
minuetchoi-study <minuetchoi@daum.net>
2023-01-11 23:24:24 +0900

    test

--- public/js/custom/custom.js
+++ public/js/custom/custom.js
@@ -626,14 +626,6 @@ function shuffle(array) {
     return array;
 }
 
-// 북마크
-function openNav() {
-    document.getElementById("mySidenav").style.width = "200px";
-}
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-}
-
 //
 
 /**
@@ -756,4 +748,23 @@ var dates = {
         var now = year + "-" + month + "-" + date + " " + hour + ":" + minute + ":" + second;
         return now
     }
+}
+// 북마크
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+}
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+// 북마크
+function openMenuNav() {
+    console.log(document.getElementById("sidebar").style.width);
+    if(document.getElementById("sidebar").style.display == 'none') { 
+        document.getElementById("sidebar").style.display = 'block';
+    } else {
+        document.getElementById("sidebar").style.display = 'none';
+    }
+}
+function closeMenuNav() {
+    document.getElementById("sidebar").style.display = 'none';
 }
